ca0f1bd621be97a233450a1696fa4ef0be90bb9c
zhangguoyong <zhangguoyong010@163.com>
2017-05-17 15:28:23 +0800

    first time

---
+++ js/Cesium-Tiles/Apps/Sandcastle/gallery/gallery-index.js
@@ -0,0 +1,323 @@
+// This file is automatically rebuilt by the Cesium build process.
+var hello_world_index = 49;
+var gallery_demos = [{
+  "name": "3D Models",
+  "date": 1493944274000,
+  "img": "3D Models.jpg"
+}, {
+  "name": "3D Models Coloring",
+  "date": 1493944274000,
+  "img": "3D Models Coloring.jpg"
+}, {
+  "name": "3D Tiles",
+  "date": 1493944274000,
+  "img": "3D Tiles.jpg"
+}, {
+  "name": "3D Tiles Batch Table Hierarchy",
+  "date": 1493944274000,
+  "img": "3D Tiles Batch Table Hierarchy.jpg"
+}, {
+  "name": "3D Tiles Point Cloud Styling",
+  "date": 1493944274000,
+  "img": "3D Tiles Point Cloud Styling.jpg"
+}, {
+  "name": "ArcGIS MapServer",
+  "date": 1493944274000,
+  "img": "ArcGIS MapServer.jpg"
+}, {
+  "name": "Atmosphere Color",
+  "date": 1493944274000,
+  "img": "Atmosphere Color.jpg"
+}, {
+  "name": "Billboards",
+  "date": 1493944274000,
+  "img": "Billboards.jpg"
+}, {
+  "name": "Box",
+  "date": 1493944274000,
+  "img": "Box.jpg"
+}, {
+  "name": "CZML",
+  "date": 1493944274000,
+  "img": "CZML.jpg"
+}, {
+  "name": "CZML Billboard and Label",
+  "date": 1493944274000,
+  "img": "CZML Billboard and Label.jpg"
+}, {
+  "name": "CZML Box",
+  "date": 1493944274000,
+  "img": "CZML Box.jpg"
+}, {
+  "name": "CZML Circles and Ellipses",
+  "date": 1493944274000,
+  "img": "CZML Circles and Ellipses.jpg"
+}, {
+  "name": "CZML Colors",
+  "date": 1493944274000,
+  "img": "CZML Colors.jpg"
+}, {
+  "name": "CZML Cones and Cylinders",
+  "date": 1493944274000,
+  "img": "CZML Cones and Cylinders.jpg"
+}, {
+  "name": "CZML Corridor",
+  "date": 1493944274000,
+  "img": "CZML Corridor.jpg"
+}, {
+  "name": "CZML Custom Properties",
+  "date": 1493944274000,
+  "img": "CZML Custom Properties.jpg"
+}, {
+  "name": "CZML Model",
+  "date": 1493944274000,
+  "img": "CZML Model.jpg"
+}, {
+  "name": "CZML Model - Node Transformations",
+  "date": 1493944274000,
+  "img": "CZML Model - Node Transformations.jpg"
+}, {
+  "name": "CZML Path",
+  "date": 1493944274000,
+  "img": "CZML Path.jpg"
+}, {
+  "name": "CZML Point",
+  "date": 1493944274000,
+  "img": "CZML Point.jpg"
+}, {
+  "name": "CZML Point - Time Dynamic",
+  "date": 1493944274000,
+  "img": "CZML Point - Time Dynamic.jpg"
+}, {
+  "name": "CZML Polygon",
+  "date": 1493944274000,
+  "img": "CZML Polygon.jpg"
+}, {
+  "name": "CZML Polygon - Interpolating References",
+  "date": 1493944274000,
+  "img": "CZML Polygon - Interpolating References.jpg"
+}, {
+  "name": "CZML Polygon - Intervals, Availability",
+  "date": 1493944274000,
+  "img": "CZML Polygon - Intervals, Availability.jpg"
+}, {
+  "name": "CZML Polyline",
+  "date": 1493944274000,
+  "img": "CZML Polyline.jpg"
+}, {
+  "name": "CZML Position Definitions",
+  "date": 1493944274000,
+  "img": "CZML Position Definitions.jpg"
+}, {
+  "name": "CZML Rectangle",
+  "date": 1493944274000,
+  "img": "CZML Rectangle.jpg"
+}, {
+  "name": "CZML Reference Properties",
+  "date": 1493944274000,
+  "img": "CZML Reference Properties.jpg"
+}, {
+  "name": "CZML Spheres and Ellipsoids",
+  "date": 1493944274000,
+  "img": "CZML Spheres and Ellipsoids.jpg"
+}, {
+  "name": "CZML Wall",
+  "date": 1493944274000,
+  "img": "CZML Wall.jpg"
+}, {
+  "name": "Camera",
+  "date": 1493944274000,
+  "img": "Camera.jpg"
+}, {
+  "name": "Camera Tutorial",
+  "date": 1493944274000,
+  "img": "Camera Tutorial.jpg"
+}, {
+  "name": "Cardboard",
+  "date": 1493944274000,
+  "img": "Cardboard.jpg"
+}, {
+  "name": "Cesium Inspector",
+  "date": 1493944274000,
+  "img": "Cesium Inspector.jpg"
+}, {
+  "name": "Cesium Widget",
+  "date": 1493944274000,
+  "img": "Cesium Widget.jpg"
+}, {
+  "name": "Circles and Ellipses",
+  "date": 1493944274000,
+  "img": "Circles and Ellipses.jpg"
+}, {
+  "name": "Clustering",
+  "date": 1493944274000,
+  "img": "Clustering.jpg"
+}, {
+  "name": "Corridor",
+  "date": 1493944274000,
+  "img": "Corridor.jpg"
+}, {
+  "name": "Custom DataSource",
+  "date": 1493944274000,
+  "img": "Custom DataSource.jpg"
+}, {
+  "name": "Custom Geocoder",
+  "date": 1493944274000,
+  "img": "Custom Geocoder.jpg"
+}, {
+  "name": "Cylinders and Cones",
+  "date": 1493944274000,
+  "img": "Cylinders and Cones.jpg"
+}, {
+  "name": "Distance Display Conditions",
+  "date": 1493944274000,
+  "img": "Distance Display Conditions.jpg"
+}, {
+  "name": "GeoJSON and TopoJSON",
+  "date": 1493944274000,
+  "img": "GeoJSON and TopoJSON.jpg"
+}, {
+  "name": "GeoJSON simplestyle",
+  "date": 1493944274000,
+  "img": "GeoJSON simplestyle.jpg"
+}, {
+  "name": "Geometry and Appearances",
+  "date": 1493944274000,
+  "img": "Geometry and Appearances.jpg"
+}, {
+  "name": "Google Earth Enterprise",
+  "date": 1493944274000,
+  "img": "Google Earth Enterprise.jpg"
+}, {
+  "name": "Ground Clamping",
+  "date": 1493944274000,
+  "img": "Ground Clamping.jpg"
+}, {
+  "name": "HeadingPitchRoll",
+  "date": 1493944274000,
+  "img": "HeadingPitchRoll.jpg"
+}, {
+  "name": "Hello World",
+  "date": 1493944274000,
+  "img": "Hello World.jpg"
+}, {
+  "name": "Imagery Adjustment",
+  "date": 1493944274000,
+  "img": "Imagery Adjustment.jpg"
+}, {
+  "name": "Imagery Layers",
+  "date": 1493944274000,
+  "img": "Imagery Layers.jpg"
+}, {
+  "name": "Imagery Layers Manipulation",
+  "date": 1493944274000,
+  "img": "Imagery Layers Manipulation.jpg"
+}, {
+  "name": "Imagery Layers Split",
+  "date": 1493944274000,
+  "img": "Imagery Layers Split.jpg"
+}, {
+  "name": "Interpolation",
+  "date": 1493944274000,
+  "img": "Interpolation.jpg"
+}, {
+  "name": "KML",
+  "date": 1493944274000,
+  "img": "KML.jpg"
+}, {
+  "name": "Labels",
+  "date": 1493944274000,
+  "img": "Labels.jpg"
+}, {
+  "name": "LocalToFixedFrame",
+  "date": 1493944274000,
+  "img": "LocalToFixedFrame.jpg"
+}, {
+  "name": "Map Pins",
+  "date": 1493944274000,
+  "img": "Map Pins.jpg"
+}, {
+  "name": "Materials",
+  "date": 1493944274000,
+  "img": "Materials.jpg"
+}, {
+  "name": "Offline",
+  "date": 1493944274000,
+  "img": "Offline.jpg"
+}, {
+  "name": "PATerrain",
+  "date": 1493944274000,
+  "img": "PATerrain.jpg"
+}, {
+  "name": "Picking",
+  "date": 1493944274000,
+  "img": "Picking.jpg"
+}, {
+  "name": "Points",
+  "date": 1493944274000,
+  "img": "Points.jpg"
+}, {
+  "name": "Polygon",
+  "date": 1493944274000,
+  "img": "Polygon.jpg"
+}, {
+  "name": "Polyline",
+  "date": 1493944274000,
+  "img": "Polyline.jpg"
+}, {
+  "name": "Polyline Dash",
+  "date": 1493944274000,
+  "img": "Polyline Dash.jpg"
+}, {
+  "name": "Polyline Volume",
+  "date": 1493944274000,
+  "img": "Polyline Volume.jpg"
+}, {
+  "name": "Projection",
+  "date": 1493944274000,
+  "img": "Projection.jpg"
+}, {
+  "name": "Rectangle",
+  "date": 1493944274000,
+  "img": "Rectangle.jpg"
+}, {
+  "name": "Rotatable 2D Map",
+  "date": 1493944274000,
+  "img": "Rotatable 2D Map.jpg"
+}, {
+  "name": "Shadows",
+  "date": 1493944274000,
+  "img": "Shadows.jpg"
+}, {
+  "name": "Show or Hide Entities",
+  "date": 1493944274000,
+  "img": "Show or Hide Entities.jpg"
+}, {
+  "name": "Spheres and Ellipsoids",
+  "date": 1493944274000,
+  "img": "Spheres and Ellipsoids.jpg"
+}, {
+  "name": "Star Burst",
+  "date": 1493944274000,
+  "img": "Star Burst.jpg"
+}, {
+  "name": "Terrain",
+  "date": 1493944274000,
+  "img": "Terrain.jpg"
+}, {
+  "name": "Terrain Exaggeration",
+  "date": 1493944274000,
+  "img": "Terrain Exaggeration.jpg"
+}, {
+  "name": "Video",
+  "date": 1493944274000,
+  "img": "Video.jpg"
+}, {
+  "name": "Wall",
+  "date": 1493944274000,
+  "img": "Wall.jpg"
+}, {
+  "name": "Web Map Service (WMS)",
+  "date": 1493944274000,
+  "img": "Web Map Service (WMS).jpg"
+}];
--- js/vge/zgy.js
+++ js/vge/zgy.js
@@ -30,6 +30,15 @@ function createModel(url) {
         duration: 0
     });
 }
+function createBillboard() {
+    viewer.entities.add({
+        position:Cesium.Cartesian3.fromDegrees(120.9184265136719, 39.84843635559082, 100),
+        billboard:{
+            image:'./img/chart2.png',
+        }
+    });
+}
+createBillboard();
 createModel("./models/Cesium_Air.gltf");
 var lastPick;
 var po;
@@ -40,6 +49,7 @@ function clickSelect() {
 
     var handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
     handler.setInputAction(function(movement) {
+        var iDiv;
         //var primitive;
         //var pickedObject = viewer.scene.pick(movement.endPosition);
         if(lastPick!=undefined){
@@ -51,10 +61,16 @@ function clickSelect() {
 
            var  primitive = pickedObject.primitive;
             if (pickedObject !== lastPick && primitive instanceof Cesium.Model) {
-                var iDiv = toast("hi");
+
                 //We don't use the entity here, but if you need to color based on
                 //some entity property, you can get to that data it here.
                 var entity = primitive.id;
+                var worldPosition = entity.position._value;
+                var screenPosition= Cesium.SceneTransforms.wgs84ToWindowCoordinates(viewer.scene, worldPosition);
+                var x = screenPosition.x;
+                var y =  screenPosition.y;
+                var content='<p><b>名称</b>:飞机</p></br> <p><b>经度</b>:120.17</p></br> <p><b>纬度</b>:39.04</p>';
+                iDiv = toast("三维模型",content,x,y);
                 syncDiv(entity,iDiv);
                 entity.model.color=Cesium.Color.YELLOW
                 lastPick = entity;
@@ -66,9 +82,22 @@ function clickSelect() {
                 //material.setValue('diffuse', Cesium.Cartesian4.fromColor(Cesium.Color.BLUE));
                 material.setValue('diffuse', Cesium.Cartesian4.fromColor(Cesium.Color.YELLOW));
                 lastPick = pickedObject;*/
+            }else if(pickedObject !== lastPick && primitive instanceof Cesium.Billboard){
+                var entity = primitive.id;
+                var worldPosition = entity.position._value;
+                var screenPosition= Cesium.SceneTransforms.wgs84ToWindowCoordinates(viewer.scene, worldPosition);
+                var x = screenPosition.x;
+                var y =  screenPosition.y;
+                var content='zz';
+                iDiv = toast("三维模型",content,x,y);
+                syncDiv(entity,iDiv);
+                entity.model.color=Cesium.Color.YELLOW
+                lastPick = entity;
+                alert("hi i'm Billboard");
             }
         } else if (lastPick) {
             isSync = false;
+            $("#toast").hide();
            /* primitive = lastPick.primitive;
             var material = primitive.getMaterial('Material_0');
             //var material = primitive.getMaterial('Red'); - original line
@@ -85,22 +114,49 @@ function syncDiv(entity,iDiv) {
     handler.setInputAction(function (movent) {
         isSync= false;
     },Cesium.ScreenSpaceEventType.LEFT_UP);
+    handler.setInputAction(function (movent) {
+        isSync= true;
+    },Cesium.ScreenSpaceEventType.MIDDLE_DOWN);
+    handler.setInputAction(function (movent) {
+        isSync= false;
+    },Cesium.ScreenSpaceEventType.MIDDLE_UP);
+    /*
+    handler.setInputAction(function (movent) {
+        isSync= true;
+    },Cesium.Cesium.KeyboardEventModifier.CTRL);
+    */
     handler.setInputAction(function(movement) {
         if(entity!=undefined && isSync){
             var worldPosition = entity.position._value;
             var screenPosition= Cesium.SceneTransforms.wgs84ToWindowCoordinates(viewer.scene, worldPosition);
             var x = screenPosition.x;
             var y =  screenPosition.y;
+            y=y-100;
             iDiv.css({'left':x+'px','top':y+'px'});
             console.log("x is:"+x+",y is:"+y);
         }
-    },Cesium.ScreenSpaceEventType.MOUSE_MOVE)
+    },Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+    handler.setInputAction(function(movement) {
+        if(entity!=undefined){
+            var worldPosition = entity.position._value;
+            var screenPosition= Cesium.SceneTransforms.wgs84ToWindowCoordinates(viewer.scene, worldPosition);
+            var x = screenPosition.x;
+            var y =  screenPosition.y;
+            y=y-100;
+            iDiv.css({'left':x+'px','top':y+'px'});
+            console.log("x is:"+x+",y is:"+y);
+        }
+    },Cesium.ScreenSpaceEventType.WHEEL)
+
 }
 clickSelect();
-function toast(content,left,top) {
-    var iDiv = $("<div>hello</div>");
-    iDiv.attr("class","toast");
+function toast(title,content,left,top) {
+    var iDiv = $("#toast");
+    $("#popTitle").text(title);
+    $("#popContent").html(content);
+    top=top-100;
     iDiv.css({'left':left+'px','top':top+'px'});
-    $("#cesiumContainer").append(iDiv);
+    iDiv.show();
+    //$("#cesiumContainer").append(iDiv);
     return iDiv;
 }
